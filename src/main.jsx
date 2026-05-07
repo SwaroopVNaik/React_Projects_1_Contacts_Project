06a30da44d3b55a39c7709cd00ab19ce52cb3340
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-07 22:17:33 +0530

    Main_Root

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
+
+// StrictMode -> it is a react helper tool it checks 
+// --> The code warning and helps the developer write 
+// clean code...
+
+// createRoot => connect react with HTML Page 
+// (react needs a place to display UI Page)
+
