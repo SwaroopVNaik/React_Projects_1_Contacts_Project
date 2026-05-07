aebf74e154b60c387e6175ec0d98690c97b5afe2
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-07 23:08:41 +0530

    Main_file_bootstrap_added

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
@@ -14,4 +15,3 @@ createRoot(document.getElementById("root")).render(
 
 // createRoot => connect react with HTML Page 
 // (react needs a place to display UI Page)
-
