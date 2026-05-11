b00abaca9a3c4e907c92c6c80895c97c626fc170
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-11 20:47:54 +0530

    main_file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
