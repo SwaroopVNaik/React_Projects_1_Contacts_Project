7fccadeb86af83979d8916ce5edd8adde1dc4fec
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-13 10:37:06 +0530

    components

--- src/Contact_App/Contact_list.jsx
+++ src/Contact_App/Contact_list.jsx
@@ -1,29 +1,32 @@
-import React from "react";
-let Contact_list = (props) => {
-    return <div>
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>User Id</th>
-                    <th>Name</th>
-                    <th>City</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    props.users.map((user, index) => (
-                        <tr key={index}>
-                            <td>{user.login.uuid.slice(-8)}</td>
-                            <td>{user.name.first}</td>
-                            <td>{user.location.city}</td>
-                            <td>{user.email}</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
-    </div>
+let ContactList = (props)=>{
+    let SelContactHandler=(user)=>{
+        //console.log(user.name.first)
+        props.getSelContact(user);
+    }
+    return <div className="mt-4">
+                <h2 className="mb-3 text-primary">Contact List</h2>
+                <table className="table table-striped table-hover table-bordered">
+                    <thead className="table-dark">
+                    <tr>
+                        <th className="text-white">User Id</th>
+                        <th className="text-white">Name</th>
+                        <th className="text-white">City</th>
+                        <th className="text-white">Email</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        props.users.map((user,index)=>{
+                            return <tr key={index} onMouseOver={SelContactHandler.bind(null,user)}>
+                                        <td>{user.login.uuid.substring(32)}</td>
+                                        <td>{user.name.first}</td>
+                                        <td>{user.location.city}</td>
+                                        <td>{user.email}</td>
+                                    </tr>
+                        })
+                    }
+                    </tbody>
+                </table>
+            </div>
 }
-
-export default Contact_list;
+export default ContactList;
