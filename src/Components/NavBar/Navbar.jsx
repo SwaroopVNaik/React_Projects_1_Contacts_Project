5bbb8dfc9111db6da7578af82d15170021ecbd53
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-13 10:36:41 +0530

    Navbar

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
 import {Link} from 'react-router-dom'
-const Navbar = () => {
-  return    <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
-                <Link to='/index' className='navbar-brand'>Logo</Link>
+let Navbar = ()=>{
+
+    return <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+            <Link to="/index" className='navbar-brand'>Logo</Link>    
                 <div className='ms-auto'>
                     <ul className='navbar-nav'>
-                        <li><Link to="/index" className='nav-link'>Home</Link></li>
-                        <li><Link to="/about" className='nav-link'>About</Link></li>
-                        <li><Link to="/services" className='nav-link'>Services</Link></li>
-                        <li><Link to="/contact" className='nav-link'>Contact</Link></li>
+                    <li><Link to="/index" className='nav-link'>Home</Link></li>
+                    <li><Link to="/contact" className='nav-link'>Contact</Link></li>
                     </ul>
                 </div>
             </nav>
-  
 }
-export default Navbar
+
+export default Navbar;
 
 // React Router DOM is a library used in React for navigation between pages without reloading the website.
