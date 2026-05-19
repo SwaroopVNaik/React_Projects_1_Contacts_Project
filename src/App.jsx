a05b47242c0f3d9859422a1b3d8d7894c5485a7f
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-05-19 23:20:38 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import Navbar from "./Components/NavBar/Navbar";
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import Home from './components/Home';
-import ContactApp from "./Contact_App/Contact_App";
-let App = ()=>{
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './Components/Login';
 
-  return <div>
-            <Router>
-              <Navbar/>
-              <Routes>
-                  <Route path="/index" element={<Home/>}/>
-                  <Route path="/contact" element={<ContactApp/>}/>
-              </Routes>
-            </Router>   
-        </div>
-}
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+};
+
 export default App;
